5b3ae54c1d12cb6df8f6992604a83d07e6f2d39e
mrzefix23 <yanismahdi123@gmail.com>
2025-10-31 17:13:50 +0100

    feat(frontend): add user registration form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth';
+import { RegisterComponent } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'register', component: RegisterComponent }
+];
