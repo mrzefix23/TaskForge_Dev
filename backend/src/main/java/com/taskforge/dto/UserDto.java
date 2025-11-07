6fcc22d7e063b12f3b5ba77152463e3e07f11048
mrzefix23 <yanismahdi123@gmail.com>
2025-11-07 11:20:00 +0100

    chore: add Lombok annotations to UserDto

--- backend/src/main/java/com/taskforge/dto/UserDto.java
+++ backend/src/main/java/com/taskforge/dto/UserDto.java
@@ -2,9 +2,13 @@ package com.taskforge.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
     private String username;
     private String email;
