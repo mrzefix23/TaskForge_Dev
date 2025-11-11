e0c8fcd37dc252bf358d1f34caebe6647e4d0bea
mrzefix23 <yanismahdi123@gmail.com>
2025-11-11 16:21:53 +0100

    Add Accueil component

---
+++ frontend/src/app/components/accueil/accueil.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  standalone: true,
+  templateUrl: './accueil.html',
+  styleUrl: './accueil.css'
+})
+export class AccueilComponent {}
