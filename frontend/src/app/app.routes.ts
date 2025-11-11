10795f55359fb6d3ade1b9c2d22edbdd6091261b
mrzefix23 <yanismahdi123@gmail.com>
2025-11-11 16:21:11 +0100

    Add authentication guard to protect routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth';
 import { RegisterComponent } from './components/register/register';
 import { LoginComponent } from './components/login/login';
+import { AccueilComponent } from './components/accueil/accueil';
+import { AuthGuard } from './auth.guard';
+import { NotFoundComponent } from './components/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'accueil', component: AccueilComponent, canActivate: [AuthGuard] },
+  { path: 'error', component: NotFoundComponent }
 ];
