42ddc3221f9b674e3c59ce0b17c005ec00cb0af0
Noa <noa.paris@etu.u-bordeaux.fr>
2025-11-12 07:41:57 +0100

    kanban files added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth';
 import { RegisterComponent } from './components/register/register';
 import { LoginComponent } from './components/login/login';
+import { Kanban } from './components/kanban/kanban';
 
 export const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'kanban', component: Kanban }
 ];
